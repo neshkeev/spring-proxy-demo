4d51dcfe907b08804b847eae04b356297aa09e6b
Nikita Eshkeev <neshkeev@yandex.ru>
2023-07-25 12:59:14 +0300

    Initial Placement of the project
    Signed-off-by: Nikita Eshkeev <neshkeev@yandex.ru>

---
+++ src/main/java/com/github/neshkeev/spring/proxy/simple/HelloWorldProxy.java
@@ -0,0 +1,7 @@
+package com.github.neshkeev.spring.proxy.simple;
+
+public class HelloWorldProxy {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
